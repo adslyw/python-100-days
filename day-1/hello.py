e5dd886ccd04ae0a3ed7c36f0e4fa1c8eaf8b030
Name From Git-Plugin-Test <email.from.git.plugin.test@example.com>
2018-07-19 22:43:36 +0800

    first day

---
+++ day-1/hello.py
@@ -0,0 +1,16 @@
+#!/bin/python
+
+"""
+first python shell
+simple hello world example
+
+Version: 0.1
+Author: adslyw
+Date: 2018-07-19
+"""
+
+print('hello world!')
+print('hello', 'world')
+#print('hello', 'world', sep=', ', end='!')
+print('Bye bye!')
+
